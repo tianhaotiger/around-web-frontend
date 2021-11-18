132468383dde514fea8f2dba742f3302ab4758d8
Tianhao(Tiger) Han <tianhaotiger@163.com>
2021-11-17 21:49:20 -0500

    finished version

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const TOKEN_KEY = 'token';
+export const BASE_URL = 'https://genuine-segment-330721.uc.r.appspot.com';
+    // https://genuine-segment-330721.uc.r.appspot.com
+// https://around-dot-around-179500.uc.r.appspot.com
+
+// [https://genuine-segment-330721.uc.r.appspot.com]
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+};
